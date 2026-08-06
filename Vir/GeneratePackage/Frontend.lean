989e26b5551d06effd8896d2994ba4d1fbb3c687
Emilio Jesús Gallego Arias <emilio@lean-fro.org>
2026-08-06 17:40:30 +0200

    refactor: unify module-safe interface validation (#105)

--- Vir/GeneratePackage/Frontend.lean
+++ Vir/GeneratePackage/Frontend.lean
@@ -4,6 +4,9 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: Emilio J. Gallego Arias
 -/
 
+import Lean.Compiler.InitAttr
+import Lean.Elab.Frontend
+import Lean.LabelAttribute
 import Vir.GeneratePackage.Basic
 
 open Lean
--- Vir/GeneratePackage/Interface/Classify/Core.lean
+++ Vir/GeneratePackage/Interface/Classify/Core.lean
@@ -4,7 +4,11 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: Emilio J. Gallego Arias
 -/
 
-import Vir.GeneratePackage.Interface.Classify.Basic
+module
+
+public import Vir.GeneratePackage.Interface.Classify.Basic
+
+public section
 
 open Lean
 
